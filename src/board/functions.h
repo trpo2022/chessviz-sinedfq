6e6493d944d9ea33c074f6f0dfa45a53582cb34e
chessviz-sinedfq <de@Hp_Denis.localdomain>
2022-04-21 21:40:26 +0700

    Add new functional

--- src/board/functions.h
+++ src/board/functions.h
@@ -1,3 +1,5 @@
 #pragma once
+
 void swap(int size, char a[size][size], int n,  char firstpos[n], char secondpos[n]);
 void chess(char a[][9]);
+void checkMove(int size, char a[size][size], int n, char firstpos[n], int SsecondIndex, int FsecondIndex)
