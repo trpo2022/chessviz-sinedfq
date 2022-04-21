e0b3d240238fb0bf0597193ac4ab0c4b8931e889
chessviz-sinedfq <de@Hp_Denis.localdomain>
2022-04-21 22:08:06 +0700

    Add second test

--- test/board_test.c
+++ test/board_test.c
@@ -1,6 +1,8 @@
 #include "ctest.h"
 #include <stdio.h>
 #include "../src/board/functions.h"
+#include "../src/board/functions.c"
+#define SIZE 9
 
 CTEST(read_txt, simple_read_file)
 {
@@ -12,3 +14,27 @@ CTEST(read_txt, simple_read_file)
     ASSERT_STR(result, expected);
 }
 
+CTEST(read_txt, checkMoveCorrect)
+{
+    FILE* file = fopen("test.txt", "r");
+    char positions[8];
+    fgets(positions, 8, file);
+    fclose(file);
+    
+    int ALetter = 96; // A - ASCII
+    int ANumber = 56;
+    const int res_error = 1; 
+    int size = 9;
+    char a[size][size];
+    chess(a);
+
+    int startFirstIndex = positions[0] - ALetter;
+    int startSecondIndex = 8 - ((positions[1] - ANumber)*(-1));
+    
+    int finalFirstIndex = positions[3] - ALetter;
+    int finalSecondIndex = 8 - ((positions[4] - ANumber)*(-1));
+
+    int temp = checkMove(size, a, 8, positions[8], startSecondIndex, finalSecondIndex);
+    
+    ASSERT_STR(res_error, temp);
+}
