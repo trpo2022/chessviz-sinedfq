dc780ad7bb4e0012e6187dc323b229c21bda27d5
chessviz-sinedfq <de@Hp_Denis.localdomain>
2022-04-22 16:34:40 +0700

    Add CTEST functions

--- test/board_test.c
+++ test/board_test.c
@@ -1,8 +1,8 @@
-#include "ctest.h"
+#include "../thirdparty/ctest.h"
 #include <stdio.h>
 #include "../src/board/functions.h"
 #include "../src/board/functions.c"
-#define SIZE 9
+
 
 CTEST(read_txt, simple_read_file)
 {
@@ -10,7 +10,9 @@ CTEST(read_txt, simple_read_file)
     char result[6];
     fgets(result, 6, file);
     fclose(file);
+
     const char expected[6] = {'a', '7', 'a', '6'};
+
     ASSERT_STR(result, expected);
 }
 
@@ -21,22 +23,19 @@ CTEST(read_txt, checkMoveCorrect)
     fgets(positions, 8, file);
     fclose(file);
     
-    int ALetter = 96; // A - ASCII
     int ANumber = 56;
     const int res_error = 0; 
     int size = 9;
     char a[size][size];
     chess(a);
 
-    int startFirstIndex = positions[0] - ALetter;
     int startSecondIndex = 8 - ((positions[1] - ANumber)*(-1));
     
-    int finalFirstIndex = positions[3] - ALetter;
     int finalSecondIndex = 8 - ((positions[4] - ANumber)*(-1));
 
-    int temp = checkMove(size, a, 8, positions[8], startSecondIndex, finalSecondIndex);
+    int temp = checkMove(8, positions, startSecondIndex, finalSecondIndex);
     
-    ASSERT_STR(res_error, temp);
+    ASSERT_EQUAL(res_error, temp);
 }
 
 CTEST(read_txt, checkMoveUncorrect)
@@ -46,20 +45,19 @@ CTEST(read_txt, checkMoveUncorrect)
     fgets(positions, 8, file);
     fclose(file);
     
-    int ALetter = 96; // A - ASCII
     int ANumber = 56;
     const int res_error = 1; 
     int size = 9;
     char a[size][size];
     chess(a);
 
-    int startFirstIndex = positions[0] - ALetter;
+
     int startSecondIndex = 8 - ((positions[1] - ANumber)*(-1));
     
-    int finalFirstIndex = positions[3] - ALetter;
+
     int finalSecondIndex = 8 - ((positions[4] - ANumber)*(-1));
 
-    int temp = checkMove(size, a, 8, positions[8], startSecondIndex, finalSecondIndex);
+    int temp = checkMove(8, positions, startSecondIndex, finalSecondIndex);
     
-    ASSERT_STR(res_error, temp);
+    ASSERT_EQUAL(res_error, temp);
 }
