f7c0adfadad073af9e230261898dadebd2650430
chessviz-sinedfq <de@Hp_Denis.localdomain>
2022-05-05 14:55:08 +0700

    Add new and fixed functions

--- src/board/functions.h
+++ src/board/functions.h
@@ -2,4 +2,5 @@
 
 void swap(int size, char a[size][size], int n,  char firstpos[n], char secondpos[n]);
 void chess(char a[][9]);
-int checkMove(int n, char firstpos[n], int SsecondIndex, int FsecondIndex);
+int checkMove(int startFirstIndex, int startSecondIndex);
+int check(int FirstIndex, int startSecond);
